a57efc4f387f85a6eaa4839cd10534145917d220
viniciusalkimin <vgoncalves.deal@alelo.com.br>
2023-11-10 22:09:39 -0300

    creation of project base

---
+++ src/main/java/com/alkimin/jdbipractice/JdbiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.alkimin.jdbipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkimin/jdbipractice/JdbiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkimin.jdbipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
